d94786ce3bc2fee3dc89136ff8cad2afd3ae91f6
Yuruki <me@yuruki.net>
2023-10-23 19:18:19 -0700

    Print to center of screen

--- snake/main.c
+++ snake/main.c
@@ -1,5 +1,35 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+#include <unistd.h>
+#include <sys/ioctl.h>
+
+#include <snake/term_util.h>
 
 int main() {
+    enable_raw_terminal();
+
+    struct winsize sz;
+    ioctl(1, TIOCGWINSZ, &sz);
+
+    char* hello = "My cursor is here.";
+
+    print_escape_code("[2J");
+    print_escape_code("[1;1H");
+    printf("\33[%d;%dH", sz.ws_row/2, sz.ws_col/2 - (int)strlen(hello)/2);
+    fflush(stdout);
+
+    printf("%s\r", hello);
+    fflush(stdout);
+
+    char c = '1';
+    while (c != 'q') {
+        read(STDIN_FILENO, &c, 1);
+    }
+
+    print_escape_code("[2K");
+    printf("\33[%d;%dH", sz.ws_row, 1);
+    printf("Farewell...\r\n");
     return 0;
 }
