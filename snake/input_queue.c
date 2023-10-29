dfba9c35c3e86b10be495eed4f2b179ff719f53f
Yuruki <me@yuruki.net>
2023-10-29 15:44:52 -0700

    InputQueue only pushes non-duplicate objects

--- snake/input_queue.c
+++ snake/input_queue.c
@@ -7,7 +7,7 @@ InputQueue* input_queue_init(int capacity, char initial) {
     iq->array[0] = initial;
     iq->capacity = capacity;
     iq->size = 1;
-    iq->head = 1;
+    iq->head = 0;
     iq->tail = 0;
     return iq;
 }
@@ -17,13 +17,12 @@ void input_queue_delete(InputQueue* iq) {
 }
 void input_queue_push(InputQueue* iq, char c) {
     if (iq->size == iq->capacity) return;
-
-    iq->array[iq->head] = c;
-    iq->head += 1;
+    if (iq->array[iq->head] == c) return;
     iq->size += 1;
-
+    iq->head += 1;
     if (iq->head >= iq->capacity)
         iq->head = 0;
+    iq->array[iq->head] = c;
 }
 char input_queue_pop(InputQueue* iq) {
     char c = iq->array[iq->tail];
